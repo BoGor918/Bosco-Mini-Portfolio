f253862fc2ba0be540a0ffb764bd0d1f0105a28e
BoGor918 <boscocheung1234@gmail.com>
2023-07-25 14:19:14 +0800

    Project Setup

---
+++ bosco-personal-website/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className='flex justify-center text-xl font-bold'>
+        <h1>Bosco's Personal Website</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
