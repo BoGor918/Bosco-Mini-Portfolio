95adf08f7f0b624f1d9ae0a16a76e05b396dfce4
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-08-04 07:25:25 +0800

    good

--- bosco-personal-website/src/components/home/components/MiddleComponent.tsx
+++ bosco-personal-website/src/components/home/components/MiddleComponent.tsx
@@ -4,61 +4,61 @@ export default function MiddleComponent() {
     return (
         <div className="ml-[30px] mt-[2rem] mb-[3rem] w-full">
             <div className="flex overflow-x-scroll scroll-smooth">
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
                     <span className="self-center mt-[0.5rem]">Hobbie 01</span>
                 </div>
-                <div className="flex flex-col mx-[1.2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
+                <div className="flex flex-col mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
                         <img className="rounded-full" src={Icon} alt="img" width={95} />
                     </button>
--- bosco-personal-website/src/components/login/Login.tsx
+++ bosco-personal-website/src/components/login/Login.tsx
@@ -1,20 +1,4 @@
-import { auth } from "../../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from 'react-router-dom'
-
 export default function Login() {
-  const navigate = useNavigate();
-
-  const login = () => {
-    const email = prompt("Email:", "") || "";
-    const password = prompt("Password:", "") || "";
-
-    signInWithEmailAndPassword(auth, email, password);
-
-    navigate("/");
-  };
-
-  login();
   return (
     <></>
   )
