201fde946c011e98b5a94e2270e65c4fee9e4c9e
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-07 12:02:06 +0800

    Form set up

--- bosco-personal-website/src/components/modal/AddCompanyModalComponent.tsx
+++ bosco-personal-website/src/components/modal/AddCompanyModalComponent.tsx
@@ -1,85 +1,116 @@
-import { TextInput, Button } from '@mantine/core';
-// import { DateInput } from '@mantine/dates';
+import { TextInput, Button, Checkbox, FileInput } from '@mantine/core';
+import { DateInput } from '@mantine/dates';
 
 export default function AddCompanyModalComponent() {
   return (
     <div className='flex flex-col font-light'>
       <div className='flex'>
+        {/* start date field */}
+        <DateInput
+          dateParser={(input: any) => {
+            if (input === 'WW2') {
+              return new Date(2001, 9, 18);
+            }
+            return new Date(input);
+          }}
+          valueFormat="DD/MM/YYYY"
+          label="Start Date"
+          size='md'
+          className='w-full my-[0.8rem] mr-3'
+          required
+        />
+        {/* end date field */}
+        <DateInput
+          dateParser={(input: any) => {
+            if (input === 'WW2') {
+              return new Date(2001, 9, 18);
+            }
+            return new Date(input);
+          }}
+          valueFormat="DD/MM/YYYY"
+          label="End Date"
+          size='md'
+          className='w-full my-[0.8rem] ml-3'
+          required
+        />
+      </div>
+      <div>
+        {/* check box field */}
+        <Checkbox
+          label="Present Work on This Job"
+          size='md'
+          className='w-full my-[0.8rem]'
+        />
+      </div>
+      <div className='flex'>
         {/* company field */}
         <TextInput
-          className='w-full my-[0.2rem] mr-3'
+          className='w-full my-[0.8rem] mr-3'
           size="md"
           label="Company Name"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
         {/* team field */}
         <TextInput
-          className='w-full my-[0.2rem] ml-3'
+          className='w-full my-[0.8rem] ml-3'
           size="md"
           label="Team"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
       </div>
       <div className='flex'>
         {/* position field */}
         <TextInput
-          className='w-full my-[0.2rem] mr-3'
+          className='w-full my-[0.8rem] mr-3'
           size="md"
           label="Position"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
         {/* job duties field */}
         <TextInput
-          className='w-full my-[0.2rem] ml-3'
+          className='w-full my-[0.8rem] ml-3'
           size="md"
           label="Job Duties"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
       </div>
       <div className='flex'>
         {/* projects field */}
         <TextInput
-          className='w-full my-[0.2rem] mr-3'
+          className='w-full my-[0.8rem] mr-3'
           size="md"
           label="Projects"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
         {/* skill sets field */}
         <TextInput
-          className='w-full my-[0.2rem] ml-3'
+          className='w-full my-[0.8rem] ml-3'
           size="md"
           label="Skill Sets"
           inputWrapperOrder={['label', 'error', 'input', 'description']}
+          required
         />
       </div>
       <div className='flex'>
-        {/* start date field */}
-        <TextInput
-          className='w-full my-[0.2rem] mr-3'
+        {/* company logo */}
+        <FileInput
+          className='my-[0.8rem]'
+          placeholder='Select Image'
           size="md"
-          label="Years"
-          inputWrapperOrder={['label', 'error', 'input', 'description']}
+          label="Compahny Logo"
+          withAsterisk
+          accept="image/*"
+          required
         />
-        {/* <DateInput
-          dateParser={(input: any) => {
-            if (input === 'WW2') {
-              return new Date(1939, 8, 1);
-            }
-            return new Date(input);
-          }}
-          valueFormat="DD/MM/YYYY"
-          label="Type WW2"
-          placeholder="Type WW2"
-          maw={400}
-          mx="auto"
-        /> */}
-        {/* end date field */}
-        <TextInput
-          className='w-full my-[0.2rem] ml-3'
-          size="md"
-          label="Logo"
-          inputWrapperOrder={['label', 'error', 'input', 'description']}
-        />
+      </div>
+      <div className='flex justify-center'>
+        {/* submit button */}
+        <Button size='md' className='bg-[#4094F4] w-[300px] my-[0.8rem]'>Add Work</Button>
       </div>
     </div>
   )
