44f689ab6aa8c1b4cc45f8b281b4a85c088c3ed4
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-08 16:53:22 +0800

    Update CompanyGrid.tsx

--- bosco-personal-website/src/components/grids/CompanyGrid.tsx
+++ bosco-personal-website/src/components/grids/CompanyGrid.tsx
@@ -35,7 +35,7 @@ export default function CompanyGrid() {
             <div className='mx-2 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-1 sm:gap-1 md:gap-1 lg:gap-2'>
                 {companyData.map((company: any, i: any) => (
                     <div key={i} onClick={() => openModal(company)} className='flex justify-center items-center shadow-md w-[110px] sm:w-[110px] md:w-[110px] lg:w-[292px] h-[110px] sm:h-[110px] md:h-[110px] lg:h-[292px] cursor-pointer'>
-                        <img className='w-full p-5' src={company.Logo} alt={company.CompanyName} />
+                        <img className='w-full p-2 sm:p-2 md:p-2 lg:p-5' src={company.Logo} alt={company.CompanyName} />
                     </div>
                 ))}
             </div>
