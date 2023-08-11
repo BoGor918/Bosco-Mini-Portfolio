92f6a12f3b9517dd4698b6dd372e1548164688f0
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-11 14:44:25 +0800

    update

--- bosco-personal-website/src/components/grids/EduGrid.tsx
+++ bosco-personal-website/src/components/grids/EduGrid.tsx
@@ -30,9 +30,9 @@ export default function EduGrid() {
 
     return (
         <div className='animate-fade animate-delay-0 animate-once flex justify-center items-center mt-[1rem] sm:mt-[1rem] md:mt-[1rem] lg:mt-[2rem] mb-[5rem]'>
-            <div className='mx-2 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-1 sm:gap-1 md:gap-1 lg:gap-2'>
+            <div className='mx-0 sm:mx-0 md:mx-0 lg:mx-2 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-1'>
                 {schoolData.map((school: any, i: any) => (
-                    <div key={i} onClick={() => openModal(school)} className='flex justify-center items-center shadow-md w-[110px] sm:w-[110px] md:w-[110px] lg:w-[292px] h-[110px] sm:h-[110px] md:h-[110px] lg:h-[292px] cursor-pointer'>
+                    <div key={i} onClick={() => openModal(school)} className='shadow-md flex justify-center items-center h-[115.66px] sm:h-[115.66px] md:h-[115.66px] lg:h-[298px] cursor-pointer'>
                         <img className='w-full p-4 sm:p-4 md:p-4 lg:p-10' src={school.Logo} alt={school.SchoolName} />
                     </div>
                 ))}
