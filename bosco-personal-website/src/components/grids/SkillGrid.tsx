b00ae37e46572b75cbf388fa7fcb2117cf17acd0
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-11 11:00:50 +0800

    Update SkillGrid.tsx

--- bosco-personal-website/src/components/grids/SkillGrid.tsx
+++ bosco-personal-website/src/components/grids/SkillGrid.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { MapperContext } from '../../globalVariable/MapperContextProvider';
+import { Tooltip } from '@mantine/core';
 
 export default function SkillGrid() {
     const {
@@ -10,9 +11,11 @@ export default function SkillGrid() {
         <div className='animate-fade animate-delay-0 animate-once flex justify-center items-center mt-[1rem] sm:mt-[1rem] md:mt-[1rem] lg:mt-[2rem] mb-[5rem]'>
             <div className='mx-2 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-1 sm:gap-1 md:gap-1 lg:gap-4'>
                 {skillData.map((skill: any, i: any) => (
-                    <div key={i} className='flex justify-center items-center shadow-md w-[110px] sm:w-[110px] md:w-[110px] lg:w-[175px] h-[110px] sm:h-[110px] md:h-[110px] lg:h-[175px] cursor-pointer'>
-                        <img className='w-full p-4 sm:p-4 md:p-4 lg:p-10' src={skill.Logo} alt={skill.ProjectName} />
-                    </div>
+                    <Tooltip label={skill.SkillName}>
+                        <div key={i} className='flex justify-center items-center shadow-md w-[110px] sm:w-[110px] md:w-[110px] lg:w-[175px] h-[110px] sm:h-[110px] md:h-[110px] lg:h-[175px] cursor-pointer'>
+                            <img className='w-full p-4 sm:p-4 md:p-4 lg:p-10' src={skill.Logo} alt={skill.ProjectName} />
+                        </div>
+                    </Tooltip>
                 ))}
             </div>
         </div>
