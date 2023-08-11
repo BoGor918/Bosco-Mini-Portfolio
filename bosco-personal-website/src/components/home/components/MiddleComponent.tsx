9ff3ceebb9ead955b89a25223c60504422f4a4c4
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-11 12:03:01 +0800

    Update MiddleComponent.tsx

--- bosco-personal-website/src/components/home/components/MiddleComponent.tsx
+++ bosco-personal-website/src/components/home/components/MiddleComponent.tsx
@@ -12,33 +12,33 @@ export default function MiddleComponent() {
             <div className="flex overflow-x-scroll scroll-smooth no-scrollbar">
                 <div className="flex flex-col mx-[0.8em] sm:mx-[0.8em] md:mx-[0.8em] lg:mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button onClick={() => openURL("https://www.instagram.com/cheungtszlai/")} className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
-                        <BsInstagram className="p-[20px] text-[75px] sm:text-[75px] md:text-[75px] lg:text-[95px]" color="#9A9A9A" />
+                        <BsInstagram className="p-[20px] text-[67px] sm:text-[67px] md:text-[67px] lg:text-[75px]" color="#9A9A9A" />
                     </button>
-                    <span className="self-center mt-[0.5rem] text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px]">Instragm</span>
+                    <span className="self-center mt-[0.5rem] text-[12px] sm:text-[12px] md:text-[12px] lg:text-[14px]">Instragm</span>
                 </div>
                 <div onClick={() => openURL("https://www.linkedin.com/in/tsz-lai-bosco-cheung-2476791b2/")} className="flex flex-col mx-[0.8em] sm:mx-[0.8em] md:mx-[0.8em] lg:mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
-                        <BsLinkedin className="p-[20px] text-[75px] sm:text-[75px] md:text-[75px] lg:text-[95px]" color="#9A9A9A" />
+                        <BsLinkedin className="p-[20px] text-[67px] sm:text-[67px] md:text-[67px] lg:text-[75px]" color="#9A9A9A" />
                     </button>
-                    <span className="self-center mt-[0.5rem] text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px]">LinkedIn</span>
+                    <span className="self-center mt-[0.5rem] text-[12px] sm:text-[12px] md:text-[12px] lg:text-[14px]">LinkedIn</span>
                 </div>
                 <div onClick={() => openURL("https://github.com/BoGor918")} className="flex flex-col mx-[0.8em] sm:mx-[0.8em] md:mx-[0.8em] lg:mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
-                        <BsGithub className="p-[20px] text-[75px] sm:text-[75px] md:text-[75px] lg:text-[95px]" color="#9A9A9A" />
+                        <BsGithub className="p-[20px] text-[67px] sm:text-[67px] md:text-[67px] lg:text-[75px]" color="#9A9A9A" />
                     </button>
-                    <span className="self-center mt-[0.5rem] text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px]">GitHub</span>
+                    <span className="self-center mt-[0.5rem] text-[12px] sm:text-[12px] md:text-[12px] lg:text-[14px]">GitHub</span>
                 </div>
                 <div onClick={() => openURL("https://wa.link/mthj8s")} className="flex flex-col mx-[0.8em] sm:mx-[0.8em] md:mx-[0.8em] lg:mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
-                        <BsWhatsapp className="p-[20px] text-[75px] sm:text-[75px] md:text-[75px] lg:text-[95px]" color="#9A9A9A" />
+                        <BsWhatsapp className="p-[20px] text-[67px] sm:text-[67px] md:text-[67px] lg:text-[75px]" color="#9A9A9A" />
                     </button>
-                    <span className="self-center mt-[0.5rem] text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px]">WhatsApp</span>
+                    <span className="self-center mt-[0.5rem] text-[12px] sm:text-[12px] md:text-[12px] lg:text-[14px]">WhatsApp</span>
                 </div>
                 <div onClick={() => openURL("mailto:cheungtszlai@gmail.com")} className="flex flex-col mx-[0.8em] sm:mx-[0.8em] md:mx-[0.8em] lg:mx-[2rem]" style={{ flexBasis: "auto", flexGrow: 0, flexShrink: 0 }}>
                     <button className="border-[1.5px] border-[#9a9a9a62] rounded-full p-[2px]">
-                        <BsEnvelope className="p-[20px] text-[75px] sm:text-[75px] md:text-[75px] lg:text-[95px]" color="#9A9A9A" />
+                        <BsEnvelope className="p-[20px] text-[67px] sm:text-[67px] md:text-[67px] lg:text-[75px]" color="#9A9A9A" />
                     </button>
-                    <span className="self-center mt-[0.5rem] text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px]">Email</span>
+                    <span className="self-center mt-[0.5rem] text-[12px] sm:text-[12px] md:text-[12px] lg:text-[14px]">Email</span>
                 </div>
             </div>
         </div>
