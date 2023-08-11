2f42a89d6db33fab446a60d93891a6e32bfa912e
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-11 17:32:39 +0800

    Update TopComponent.tsx

--- bosco-personal-website/src/components/home/components/TopComponent.tsx
+++ bosco-personal-website/src/components/home/components/TopComponent.tsx
@@ -54,10 +54,13 @@ export default function TopComponent() {
     useEffect(() => {
         const handleThemeColorChange = () => {
             const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+            
             if (themeColorMeta && theme === 'dark') {
                 themeColorMeta.setAttribute('content', '#0B1A33'); // Set the new theme color
+                document.body.style.backgroundColor = '#0B1A33';
             } else if (themeColorMeta && theme === 'light') {
                 themeColorMeta?.setAttribute('content', '#FFFFFF'); // Set the new theme color
+                document.body.style.backgroundColor = '#FFFFFF';
             }
         };
         handleThemeColorChange();
