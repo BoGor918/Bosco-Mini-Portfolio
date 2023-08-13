e0db5c68304baa4e3198805ad32638d851e8eea2
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-08-13 23:48:22 +0800

    Update IntroductionModalComponent.tsx

--- bosco-personal-website/src/components/modal/introduction/IntroductionModalComponent.tsx
+++ bosco-personal-website/src/components/modal/introduction/IntroductionModalComponent.tsx
@@ -1,11 +1,11 @@
 export default function IntroductionModalComponent() {
   // style variable
-  const textStyle = 'text-justify text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px] text-[#9A9A9A] dark:text-[#94A3B8]'
+  const textStyle = 'mt-[0.5rem] text-justify text-[14px] sm:text-[14px] md:text-[14px] lg:text-[16px] text-[#9A9A9A] dark:text-[#94A3B8]'
 
   return (
     <div className='flex flex-col font-light p-3'>
       {/* headline */}
-      <span className='font-medium text-black dark:text-white text-[20px] sm:text-[20px] md:text-[20px] lg:text-[25px]'>
+      <span className='text-center font-medium text-black dark:text-white text-[20px] sm:text-[20px] md:text-[20px] lg:text-[25px]'>
         Welcome To My Mini Portfolio Website
       </span>
       {/* description */}
