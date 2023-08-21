0eeb5db865db0f7bf1029dfdd18f6122449d1bad
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-21 10:30:42 +0800

    heer

--- bosco-personal-website/src/components/home/components/TopComponent.tsx
+++ bosco-personal-website/src/components/home/components/TopComponent.tsx
@@ -75,17 +75,18 @@ export default function TopComponent() {
 
     // set theme color to meta content and body background color
     useEffect(() => {
-        const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+        const handleThemeColorChange = () => {
+            const themeColorMeta = document.querySelector('meta[name="theme-color"]');
 
-        if (theme === 'dark') {
-            themeColorMeta?.setAttribute('content', '#0B1A33'); // Set the new theme color
-            document.body.style.backgroundColor = '#0B1A33';
-        } 
-
-        if (theme === 'light') {
-            themeColorMeta?.setAttribute('content', '#FFFFFF'); // Set the new theme color
-            document.body.style.backgroundColor = '#FFFFFF';
-        }
+            if (themeColorMeta && theme === 'dark') {
+                themeColorMeta.setAttribute('content', '#0B1A33'); // Set the new theme color
+                document.body.style.backgroundColor = '#0B1A33';
+            } else if (themeColorMeta && theme === 'light') {
+                themeColorMeta?.setAttribute('content', '#FFFFFF'); // Set the new theme color
+                document.body.style.backgroundColor = '#FFFFFF';
+            }
+        };
+        handleThemeColorChange();
     }, [theme]);
 
     return (
