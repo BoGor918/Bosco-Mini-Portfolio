9360ed88b7a65eecf842469a86e701da9baa984f
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-21 11:59:39 +0800

    Update TopComponent.tsx

--- bosco-personal-website/src/components/home/components/TopComponent.tsx
+++ bosco-personal-website/src/components/home/components/TopComponent.tsx
@@ -60,9 +60,7 @@ export default function TopComponent() {
     useEffect(() => {
         if (theme === "dark") {
             document.documentElement.classList.add("dark");
-            document.documentElement.classList.remove("light");
         } else {
-            document.documentElement.classList.add("light");
             document.documentElement.classList.remove("dark");
         }
 
