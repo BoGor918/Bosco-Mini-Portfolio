fda1c8745c87c172180943db12c5b46ec43d2e7d
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-21 10:25:23 +0800

    df

--- bosco-personal-website/src/components/home/components/TopComponent.tsx
+++ bosco-personal-website/src/components/home/components/TopComponent.tsx
@@ -81,12 +81,23 @@ export default function TopComponent() {
     //         themeColorMeta?.setAttribute('content', '#0B1A33'); // Set the new theme color
     //         document.body.style.backgroundColor = '#0B1A33';
     //     } 
-        
+
     //     if (theme === 'light') {
     //         themeColorMeta?.setAttribute('content', '#FFFFFF'); // Set the new theme color
     //         document.body.style.backgroundColor = '#FFFFFF';
     //     }
     // }, [theme]);
+    useEffect(() => {
+        const statusBar = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
+
+        if (theme === 'dark') {
+            statusBar?.setAttribute('content', '#0B1A33'); // Set the new theme color
+        }
+
+        if (theme === 'light') {
+            statusBar?.setAttribute('content', '#FFFFFF'); // Set the new theme color
+        }
+    }, [theme]);
 
     return (
         <div className='self-center w-full max-w-[365px] sm:max-w-[365px] md:max-w-[365px] lg:max-w-[910px] flex flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center font-light mt-[5rem]'>
