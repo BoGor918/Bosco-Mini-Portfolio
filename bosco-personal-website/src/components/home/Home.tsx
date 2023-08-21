8da98a18df48067a7c13f183db9ebc596ea3fe8f
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2023-08-21 22:00:26 +0800

    Update Home.tsx

--- bosco-personal-website/src/components/home/Home.tsx
+++ bosco-personal-website/src/components/home/Home.tsx
@@ -33,8 +33,8 @@ export default function Home() {
         <div className="bg-white dark:bg-[#0B1A33] flex flex-col">
             {/* page components */}
             <TopComponent />
-            <MiddleComponent />
-            <BottomComponent />
+            {/* <MiddleComponent />
+            <BottomComponent /> */}
         </div>
     );
 }
