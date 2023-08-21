cb87b498ba97def557cb5aa8f5a1bd31caecf6b4
Bosco CHEUNG <72458648+BoGor918@users.noreply.github.com>
2023-08-21 10:32:58 +0800

    Update TopComponent.tsx

--- bosco-personal-website/src/components/home/components/TopComponent.tsx
+++ bosco-personal-website/src/components/home/components/TopComponent.tsx
@@ -81,9 +81,7 @@ export default function TopComponent() {
             if (themeColorMeta && theme === 'dark') {
                 themeColorMeta.setAttribute('content', '#0B1A33'); // Set the new theme color
                 document.body.style.backgroundColor = '#0B1A33';
-            } else if (themeColorMeta && theme === 'light') {
-                themeColorMeta?.setAttribute('content', '#FFFFFF'); // Set the new theme color
-                document.body.style.backgroundColor = '#FFFFFF';
+                document.body.style.color = 'black'; // Set the text color to black
             }
         };
         handleThemeColorChange();
